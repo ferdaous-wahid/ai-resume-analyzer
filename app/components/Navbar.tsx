77a57dd1bffcadaca5b8b3a15e52cb17e86f9d8a
ferdaous-wahid <ferdaouswahid2001@gmail.com>
2025-08-19 21:40:23 +0600

    updating nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router';
 import React from 'react';
 
 const Navbar: () => React.JSX.Element = () => {
-  return <div className="navbar">
-    <Link to="/">
-      <p className="text-2xl font-bold text-gradient">RESUMIND</p>
-    </Link>
-    <Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
-  </div>;
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIN</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </div>
+  );
 };
 export default Navbar;
